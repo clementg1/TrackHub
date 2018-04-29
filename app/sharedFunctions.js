33bcba08834e8b7a972deb54b0b68bf0424d91db
Edgar Oregel <e_oregel@u.pacific.edu>
2018-04-29 16:29:01 -0700

    Added functions to check for where to redirect to to home from home.html

--- app/sharedFunctions.js
+++ app/sharedFunctions.js
@@ -1,10 +1,33 @@
+/**
+* Global Variable definitions
+**/
 var logged_in = false;
+var PATHS = {
+	landing_page: 'http://localhost:2000/landing_page.html',
+	home_page: 'http://localhost:2000/home.html'
+}
 
-
+/**
+*Set logged_in variable when a user is logged in
+**/
 function setLoggedIn(){
 	logged_in = true;
 }
 
+/**
+*Get logged_in variable
+**/
 function getLoggedIn(){
 	return logged_in;
+}
+
+/**
+*Check if logged_in to know where to redirect to home
+**/
+function redirectToHome(){
+	if (logged_in){
+		window.location = PATHS.home_page;
+	} else{
+		window.location = PATHS.landing_page;
+	}
 }
